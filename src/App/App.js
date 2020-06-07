e5dcfe45110f9ead44ffad78f1bfa036043eca50
Mitchell Schmidt <mitchellt.s@outlook.com>
2020-06-07 16:47:01 -0500

    changed api endpoint

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import NoteListNav from '../NoteListNav/NoteListNav';
-import NotePageNav from '../NotePageNav/NotePageNav';
-import NoteListMain from '../NoteListMain/NoteListMain';
-import NotePageMain from '../NotePageMain/NotePageMain';
-import AddFolder from '../AddFolder/AddFolder';
-import AddNote from '../AddNote/AddNote';
-import NotefulContext from '../NotefulContext';
-import NotefulError from '../NotefulError';
-import config from '../config';
-import './App.css';
+import React, { Component } from "react";
+import { Route, Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import NoteListNav from "../NoteListNav/NoteListNav";
+import NotePageNav from "../NotePageNav/NotePageNav";
+import NoteListMain from "../NoteListMain/NoteListMain";
+import NotePageMain from "../NotePageMain/NotePageMain";
+import AddFolder from "../AddFolder/AddFolder";
+import AddNote from "../AddNote/AddNote";
+import NotefulContext from "../NotefulContext";
+import NotefulError from "../NotefulError";
+import config from "../config";
+import "./App.css";
 
 class App extends Component {
 	state = {
@@ -25,10 +25,8 @@ class App extends Component {
 			fetch(`${config.API_ENDPOINT}/api/folders`),
 		])
 			.then(([notesRes, foldersRes]) => {
-				if (!notesRes.ok)
-					return notesRes.json().then((e) => Promise.reject(e));
-				if (!foldersRes.ok)
-					return foldersRes.json().then((e) => Promise.reject(e));
+				if (!notesRes.ok) return notesRes.json().then((e) => Promise.reject(e));
+				if (!foldersRes.ok) return foldersRes.json().then((e) => Promise.reject(e));
 
 				return Promise.all([notesRes.json(), foldersRes.json()]);
 			})
@@ -46,9 +44,7 @@ class App extends Component {
 	handleDeleteNote = (noteId) => {
 		console.log(noteId);
 		this.setState({
-			notes: this.state.notes.filter(
-				(note) => note.id !== parseInt(noteId)
-			),
+			notes: this.state.notes.filter((note) => note.id !== parseInt(noteId)),
 		});
 	};
 
@@ -67,17 +63,12 @@ class App extends Component {
 	renderNavRoutes() {
 		return (
 			<>
-				{['/', '/folder/:folderId'].map((path) => (
-					<Route
-						exact
-						key={path}
-						path={path}
-						component={NoteListNav}
-					/>
+				{["/", "/folder/:folderId"].map((path) => (
+					<Route exact key={path} path={path} component={NoteListNav} />
 				))}
-				<Route path='/note/:noteId' component={NotePageNav} />
-				<Route path='/add-folder' component={NotePageNav} />
-				<Route path='/add-note' component={NotePageNav} />
+				<Route path="/note/:noteId" component={NotePageNav} />
+				<Route path="/add-folder" component={NotePageNav} />
+				<Route path="/add-note" component={NotePageNav} />
 			</>
 		);
 	}
@@ -85,17 +76,12 @@ class App extends Component {
 	renderMainRoutes() {
 		return (
 			<>
-				{['/', '/folder/:folderId'].map((path) => (
-					<Route
-						exact
-						key={path}
-						path={path}
-						component={NoteListMain}
-					/>
+				{["/", "/folder/:folderId"].map((path) => (
+					<Route exact key={path} path={path} component={NoteListMain} />
 				))}
-				<Route path='/note/:noteId' component={NotePageMain} />
-				<Route path='/add-folder' component={AddFolder} />
-				<Route path='/add-note' component={AddNote} />
+				<Route path="/note/:noteId" component={NotePageMain} />
+				<Route path="/add-folder" component={AddFolder} />
+				<Route path="/add-note" component={AddNote} />
 			</>
 		);
 	}
@@ -110,20 +96,17 @@ class App extends Component {
 		};
 		return (
 			<NotefulContext.Provider value={value}>
-				<div className='App'>
+				<div className="App">
 					<NotefulError>
-						<nav className='App__nav'>{this.renderNavRoutes()}</nav>
+						<nav className="App__nav">{this.renderNavRoutes()}</nav>
 					</NotefulError>
-					<header className='App__header'>
+					<header className="App__header">
 						<h1>
-							<Link to='/'>Noteful</Link>{' '}
-							<FontAwesomeIcon icon='check-double' />
+							<Link to="/">Noteful</Link> <FontAwesomeIcon icon="check-double" />
 						</h1>
 					</header>
 					<NotefulError>
-						<main className='App__main'>
-							{this.renderMainRoutes()}
-						</main>
+						<main className="App__main">{this.renderMainRoutes()}</main>
 					</NotefulError>
 				</div>
 				{this.state.error && (
--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export default {
-	API_ENDPOINT: 'http://localhost:8000',
+	API_ENDPOINT: "https://young-lowlands-51480.herokuapp.com",
 };
